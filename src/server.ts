d6b95ad10985c4098226c60e390860e4c6cc40cf
Tim Kent <timdekent@gmail.com>
2023-08-19 13:40:38 -0700

    add cloudinary and env config file

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
 import { json } from "body-parser";
-require("dotenv").config();
 
 const app = express();
 
-// test route
+import { port } from "./config/config";
 
+// test route
 app.use("/", (req: Request, res: Response) => {
   res.send({ message: "hello world" });
 });
 
-app.use(json());
+// app.use(json());
 
-// app.listen(process.env.PORT, () => console.log(`Server listening port ${process.env.PORT}`));
-app.listen(4000, () => console.log(`Server listening port 4000`));
+app.listen(port, () => console.log(`Server listening port ${port}`));
